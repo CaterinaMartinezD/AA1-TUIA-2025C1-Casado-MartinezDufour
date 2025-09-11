bdf383916fa8bd7488f55adb41ae2ecbf0d2deb4
gcasado0 <casado.gustavo@gmail.com>
2025-09-11 20:41:27 -0300

    cambios al dataset inicial train_df

--- distancia.py
+++ distancia.py
@@ -24,11 +24,11 @@ def haversine(lon1, lat1, lon2, lat2):
 
     return distancia
 
-# Ejemplo con tus datos
-pickup_longitude = -73.9998168945313
-pickup_latitude = 40.7383537292481
-dropoff_longitude = -73.99951171875
-dropoff_latitude = 40.7232170104981
+# # Ejemplo con tus datos
+# pickup_longitude = -73.9998168945313
+# pickup_latitude = 40.7383537292481
+# dropoff_longitude = -73.99951171875
+# dropoff_latitude = 40.7232170104981
 
-distancia_km = haversine(pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude)
-print(f"Distancia: {distancia_km:.3f} km")
+# distancia_km = haversine(pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude)
+# print(f"Distancia: {distancia_km:.3f} km")
